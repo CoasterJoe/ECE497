a3ccbd5751c6c6af3cbd4c42edf68f46706bf909
Joseph A. Militello <militeja@rose-hulman.edu>
2016-09-13 14:47:19 -0400

    Updeated hw01

--- hw01/EtchASketch.c
+++ hw01/EtchASketch.c
@@ -36,11 +36,11 @@ int yCord = 0;
 
 //initial grid 
 int spots[width][height];
-
+int justCleared=1;
 //Repeating loop of drawing and user answering inputting
-  char response;
-  printf("Enter 'Q' to quite anytime\n");
 while(1==1){
+  char response;
+  printf("Enter 'q' to quite anytime\n");
   //update location
   spots[xCord][yCord]=1;
 
@@ -59,68 +59,58 @@ while(1==1){
   	printf("%d:",drawY);
   	for(drawX=0; drawX<width; drawX++){
   		if(spots[drawX][drawY]==1){
-  			printf("\tX");
+			if(justCleared==1){
+				spots[drawX][drawY]=0;
+				printf("\t");
+			}else{
+	  			printf("\tX");
+			}
   		}else{
   			printf("\t");
   		}
   	}
   	printf("\n");
   }
-
+ justCleared = 0;
   //Player Response
 
-  printf("Erase? (Y/N)\n");
-  scanf("%s", &response);
-  if(response =='Q'){
+
+
+//Takes user input and moves the person up, down, or neither
+  printf("Move Up 'u'\nMove Down 'd'\nMove Left 'l'\nMove Right 'r'\nErase 'e'\nor Quit 'q'\n");
+   scanf("%s", &response);
+   if(response =='q'){
   	printf("\nEnding Program\n\n");
   	return 0;
-  }
-   //If yes, Erase the map
-  if(response=='Y'){
-  	for(drawY=0; drawY<height; drawY++){
+  
+  }else if(response=='e'){
+	for(drawY=0; drawY<height; drawY++){
   		for(drawX=0; drawX<width; drawX++){
   			spots[drawX][drawY]=0;
   		
   		}
   	}
+	justCleared = 1;
   	printf("Erased Map\n");
-  }
 
-//Takes user input and moves the person up, down, or neither
-  printf("Move Up 'U' or Move Down 'D' on Neither 'N'\n");
-   scanf("%s", &response);
-   if(response =='Q'){
-  	printf("\nEnding Program\n\n");
-  	return 0;
-  }
 
-  if(response=='U'){
+  }else if(response=='u'){
   		yCord--;
   		if(yCord<0){
   			yCord = 0;
   		}
-  }else if(response=='D'){
+  }else if(response=='d'){
   		yCord++;
   		if(yCord>=height){
   			yCord = height-1;
   		}
-  }
 
-
-//Takes user input and moves the user left, right, or niether
-  printf("Move Left'L' or Move Right 'R' on Neither 'N'\n");
-   scanf("%s", &response);
-   if(response =='Q'){
-  	printf("\nEnding Program\n\n");
-  	return 0;
-  }
-
-  if(response=='L'){
+  }else if(response=='l'){
   		xCord--;
   		if(xCord<0){
   			xCord = 0;
   		}
-  }else if(response=='R'){
+  }else if(response=='r'){
   		xCord++;
   		if(xCord>=width){
   			xCord = width-1;
