dfe86420428e84ed6f74f5af1077ce778076dbd3
CoasterJoe <militeja@rose-hulman.edu>
2016-09-16 18:25:54 +0000

    Commeted, documented, and working

--- hw03/AlertTemp.js
+++ hw03/AlertTemp.js
@@ -1,25 +1,38 @@
 //Joseph Militello
 
-//9/15/16
+//9/16/16
 
 //Temp Alert
 
 //Import Bonescript
 
 var b = require('bonescript');
+var i2c = require('i2c');
+var i2c2 = require('i2c');
+var wire1 = new i2c(0x48,{
+	device: '/dev/i2c-2'
+});
+
+//Set up I2C
+var wire2 = new i2c2(0x49,{
+	device: '/dev/i2c-2'
+});
 
 
+//Set up outputs
 var alert = 'P9_11';
 var alert2 = 'P9_12';
 
 var led1 = 'P9_13';
 var led2 = 'P9_15';
 
-
+//set Up LEDs
 var state= b.HIGH;
 var state2=b.HIGH;
 var start = 0;
 
+
+//Set up Pinmodes
 b.pinMode(led1, b.OUTPUT);
 b.pinMode(led2, b.OUTPUT);
 b.pinMode(alert, b.INPUT, 7);
@@ -27,11 +40,13 @@ b.pinMode(alert2, b.INPUT,7);
 b.attachInterrupt(alert, true, b.CHANGE, turnOn);
 b.attachInterrupt(alert2, true, b.CHANGE, turnOn2);
 
-
+//Waiting For User Alert
 console.log("Waiting for Alert");
 start = 1;
 
 
+
+//Two methods to light up LED and print out temperature for each temperature sensor.
 function turnOn(){
 	console.log("Alert!");
 	if(start===0){
@@ -39,6 +54,16 @@ function turnOn(){
 	}
 	state =b.HIGH;
 	b.digitalWrite(led1, state);
+	var res = 0x00;
+	wire1.readByte(function(err,res){
+	if(err){
+
+	}else{
+		var aTemp = res*9/5+32
+		console.log(aTemp+"F");
+	}
+	});
+
 }
 
 function turnOn2(){
@@ -48,4 +73,15 @@ function turnOn2(){
 	}
 	state2= b.HIGH;
 	b.digitalWrite(led2, state2);
+ 	var res2=0x00
+	wire2.readByte(function(err,res2){
+	if(err){
+
+	}else{
+		var aTemp2 = res2*9/5+32
+		console.log(aTemp2+"F");
+	}
+	});
+
 }
+
--- hw03/temp.sh
+++ hw03/temp.sh
@@ -2,29 +2,39 @@
 # Joseph Militello
 
 
+#9/16/16
+#used to run the TEMP101 sensors
 
 echo "Running Temp program"
 
-#i2cdetect -y -r 2
 
-i2cset -y 2 0x48 02 0x19
-i2cset -y 2 0x49 02 0x19
+
+#set up i2c low and highs
+
+i2cset -y 2 0x48 02 0x15
+i2cset -y 2 0x49 02 0x15
 i2cset -y 2 0x48 03 0x1a
 i2cset -y 2 0x49 03 0x1a
 
+#set up variables
 TEMP1=$(i2cget -y 2 0x48 0x00)
 TEMP2=$(i2cget -y 2 0x49 0x00)
-TEMP1A=$(((TEMP1*9)/5+32))
-TEMP2A=$(((TEMP2*9)/5+32))
+TEMP1A=$(((TEMP1*9)/5+32))F
+TEMP2A=$(((TEMP2*9)/5+32))F
 
-
+#print out temperature
 echo "Printing Temperature"
 echo $TEMP1A
 echo $TEMP2A
 
+#set up for alarm
 i2cset -y 2 0x48 01 0x81
 i2cset -y 2 0x49 01 0x81
 
-echo "Entering Waiting For Alarm"
+HOLDER=$(i2cget -y 2 0x48 0x00)
+HOLDER2=$(i2cget -y 2 0x49 0x00)
 
+echo "Running AlertTemp.js"
+
+#running other program
 node AlertTemp.js
