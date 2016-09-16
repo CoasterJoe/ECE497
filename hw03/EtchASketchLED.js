c599388b59b3c3d2d0b49a063213bd4696a87731
CoasterJoe <militeja@rose-hulman.edu>
2016-09-16 04:08:30 +0000

    Almost working

--- hw03/EtchASketchLED.js
+++ hw03/EtchASketchLED.js
@@ -4,8 +4,8 @@
 
 //Beagle Bone Stuff
 //#!/usr/bin/env node
-var b = require('bonescript');
-
+var b =require('bonescript');
+console.log("Starting Program");
 
 //i2c stuff
 //var time = 1000; 
@@ -14,10 +14,11 @@ var matrix = 0x70;
 var port = '/dev/i2c-2';
 var LEDSize = 16;
 
-//var screen = [0x00,0xFF,0x00, 0xFF,0x00,0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00,0xFF, 0x00,0xFF, 0x00, 0xFF];
+
 var count = 0;
 var screen = new Array(16);
 
+var status = 0;
 
 
 iScreen();
@@ -43,19 +44,32 @@ var height = 8;
 
 var xCord = 0;
 var yCord = 0;
-addScreen();
 
-xCord = 0; 
-yCord = 1;
-addScreen();
+//Assign pins
+var up = 'P9_14';
+var down = 'P9_23';
+var left = 'P9_24';
+var right ='P9_26';
+var clear = 'P9_27';
 
-xCord = 0;
-yCord = 2;
-addScreen();
+//Assign Pin values
+b.pinMode(up, b.INPUT,7,'pulldown');
+b.pinMode(down,b.INPUT,7,'pulldown');
+b.pinMode(left,b.INPUT,7,'pulldown');
+b.pinMode(right,b.INPUT,7,'pulldown');
+b.pinMode(clear,b.INPUT,7,'pulldown');
 
-xCord = 0;
-yCord = 3;
-addScreen();
+//attachInterrups
+b.attachInterrupt(up,true,b.FALLING, goUp);
+b.attachInterrupt(down,true,b.FALLING, goDown);
+b.attachInterrupt(left,true,b.FALLING, goLeft);
+b.attachInterrupt(right,true,b.FALLING, goRight);
+b.attachInterrupt(clear,true,b.FALLING, clearScreen);
+
+
+status = 1;
+doScreen();
+console.log("Ready");
 
 
 function doScreen(){
@@ -70,11 +84,71 @@ function iScreen(){
 		screen[count+1]=0x00;
 	}
 }
+function clearScreen(){
+	console.log("Clear");
+	iScreen();
+	addScreen();
+}
 
 function addScreen(){
 	var holder = Math.pow(2,yCord);
 	screen[2*xCord]=screen[2*xCord]-holder;
 	screen[2*xCord+1]=holder+screen[2*xCord+1];
 	doScreen();	
+}
+
+function checkValue(){
+	if(xCord<0){
+		xCord=0;
+	}
+	if(xCord>=width){
+		xCord = width-1;
+	}
+
+	if(yCord<0){
+		yCord=0;
+	}
+	if(yCord>=height){
+		yCord=height-1;
+	}
+}
+
+
+function goUp(){
+	console.log("UP");
+	if(status===0){
+		return;
+	}	
+	yCord=yCord+1;
+	checkValue();
+	addScreen();
+}
+
+function goDown(){
+	console.log("DOWN");
+	if(status===0){
+		return;
+	}
+	yCord=yCord-1;
+	checkValue();
+	addScreen();
+}
+function goLeft(){
+	console.log("LEFT");
+	if(status===0){
+		return;
+	}	
+	xCord=xCord-1;
+	checkValue();
+	addScreen();
+}
+function goRight(){
+	console.log("RIGHT");
+	if(status===0){
+		return;
+	}
+	xCord=xCord+1;
+	checkValue();
+	addScreen();
 }
 
