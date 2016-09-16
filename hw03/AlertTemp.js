1f7d8e0b3ec1e9909de3bb7ddbfba6e6c09760c4
CoasterJoe <militeja@rose-hulman.edu>
2016-09-16 01:52:36 +0000

    More files for homework 3

---
+++ hw03/AlertTemp.js
@@ -0,0 +1,56 @@
+//Joseph Militello
+
+//9/15/16
+
+//Temp Alert
+
+//Import Bonescript
+
+var b = require('bonescript');
+
+
+var alert = 'P9_17';
+var alert2 = 'P9_18';
+
+var led1 = 'P9_13';
+var led2 = 'P9_14';
+
+
+var state= b.HIGH;
+var state2=b.HIGH;
+var start = 0;
+
+b.pinMode(led1, b.OUTPUT);
+b.pinMode(led2, b.OUTPUT);
+b.pinMode(alert, b.INPUT, 7);
+b.pinMode(alert2, b.INPUT,7);
+b.attachInterrupt(alert, true, b.CHANGE, turnOn);
+b.attachInterrupt(alert2, true, b.CHANGE, turnOn2);
+
+
+
+console.log("Waiting for Alert");
+start = 1;
+while(1==1){
+
+//b.digitalWrite(led1,b.HIGH);
+//b.digitalWrite(led2,b.HIGH);
+}
+
+function turnOn(){
+	console.log("Alert");
+	if(start===0){
+		return; 
+	}
+	state =b.HIGH;
+	b.digitalWrite(led1, state);
+}
+
+function turnOn2(){
+	console.log("Alert2");
+	if(start===0){
+		return;
+	}
+	state2= b.HIGH;
+	b.digitalWrite(led2, state2);
+}
---
+++ hw03/temp.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+# Joseph Militello
+
+
+
+echo "Running Temp program"
+
+#i2cdetect -y -r 2
+
+i2cset -y 2 0x48 01 0x80
+i2cset -y 2 0x49 01 0x80
+i2cset -y 2 0x48 02 0x19
+i2cset -y 2 0x49 02 0x19
+i2cset -y 2 0x48 03 0x1a
+i2cset -y 2 0x49 03 0x1a
+
+TEMP1=$(i2cget -y 2 0x48 0x00)
+TEMP2=$(i2cget -y 2 0x49 0x00)
+TEMP1A=$(((TEMP1*9)/5+32))
+TEMP2A=$(((TEMP2*9)/5+32))
+
+
+echo "Printing Temperature"
+echo $TEMP1A
+echo $TEMP2A
+
+echo "Entering Waiting For Alarm"
+
+node AlertTemp.js
